4953ea41ae5193b66425520e6f5afc544a96a38f
chuphucben <33323583+git-thinh@users.noreply.github.com>
2023-07-30 11:52:36 +0700

    Review 2023-07-30

---
+++ fond-end/src/components/blog/BlogNewContent.vue
@@ -0,0 +1,230 @@
+<template>
+	<div class="blogNewContent">
+		<h1 class="__blog-title">Bộ Công Thương: Chỉ khuyến khích điện mặt trời mái nhà tự dùng tại công sở, nhà ở </h1>
+		<p class="__blog-description">
+			Các bộ, ngành đề nghị mở rộng phạm vi lắp đặt điện mặt trời mái nhà tự sản tự tiêu, nhưng Bộ Công Thương nói "không".
+		</p>
+		<article class="__blog-content">
+			<p>Quan điểm này được Bộ Công Thương nêu khi báo cáo Thủ tướng về dự thảo quyết định khuyến
+				khích phát triển điện mặt trời mái nhà lắp đặt tại nhà ở, cơ quan công sở tại Việt Nam.</p>
+			<p>Theo đó, các bộ, ngành khi góp ý đều muốn mở rộng phạm vi lắp đặt hệ thống điện mặt trời
+				mái nhà tự sản tự tiêu không chỉ trên mái nhà ở, công sở, trụ sở doanh nghiệp mà cho các công trình khác
+				như trường học, bệnh viện, khu công nghiệp, khách sạn.</p>
+			<p>Các bộ cũng đề nghị làm rõ khái niệm nhà ở trong khu vực nông thôn và bổ sung hộ nông dân
+				có trang trại, nhà kho gắn liền với nhà ở, bến xe, cảng biển... được hưởng chính sách hỗ trợ lần này.
+			</p>
+			<p>Phản hồi những góp ý này, Bộ Công Thương dẫn chỉ đạo của Phó thủ tướng Trần Hồng Hà, cho
+				hay cơ chế lần này chỉ áp dụng với điện mặt trời mái nhà lắp tại nhà ở, cơ quan công sở. Việc mở rộng
+				đối tượng sẽ được nghiên cứu trong các quy định sau này.</p>
+			<figure data-size="true" itemprop="associatedMedia image" itemscope=""
+				itemtype="http://schema.org/ImageObject" class="tplCaption action_thumb_added">
+				<div class="action_thumb flexbox"
+					style="transform-origin: 0px 0px; opacity: 1; transform: scale(1, 1); z-index: 9; display: none;">
+					<a href="javascript:;" class="share_photo" data-type="fb" data-reference-id="0"><svg
+							class="ic ic-facebook">
+							<use xlink:href="#Facebook"></use>
+						</svg></a>
+					<a href="javascript:;" class="share_photo" data-type="tw" data-reference-id="0"><svg
+							class="ic ic-twitte">
+							<use xlink:href="#Twitter"></use>
+						</svg></a>
+					<a href="javascript:;" class="vne_zoom"
+						style="transform-origin: 0px 0px; opacity: 1; transform: scale(1, 1);"><svg class="ic ic-zoom">
+							<use xlink:href="#Zoom"></use>
+						</svg></a>
+				</div>
+				<meta itemprop="url"
+					content="https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=0&amp;h=0&amp;q=100&amp;dpr=2&amp;fit=crop&amp;s=55gxxJewr8I3bUs2Uz4dTA">
+				<meta itemprop="width" content="1200">
+				<meta itemprop="height" content="746">
+				<meta itemprop="href" content="">
+				<div class="fig-picture" style="padding-bottom: 62.111801242236%;position: relative;">
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+						<source
+							data-srcset="https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=iXXLmHw_Z_97SbG5_5W3ZQ 1x, https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=1020&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=WUEqxtpCj43mt5rqDto_Vg 1.5x, https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=2&amp;fit=crop&amp;s=pPExbxOD3NT5Hb2xbKDMrg 2x"
+							srcset="https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=iXXLmHw_Z_97SbG5_5W3ZQ 1x, https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=1020&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=WUEqxtpCj43mt5rqDto_Vg 1.5x, https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=2&amp;fit=crop&amp;s=pPExbxOD3NT5Hb2xbKDMrg 2x">
+						<!--[if IE 9]></video><![endif]-->
+						<img itemprop="contentUrl" style="position: absolute;width:100%;left:0;" loading="lazy"
+							intrinsicsize="680x0"
+							alt="Hệ thống điện mặt trời mái nhà được lắp trên nóc một trụ sở doanh nghiệp tại TP HCM. Ảnh: EVNHCM"
+							class="lazy lazied"
+							src="https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=iXXLmHw_Z_97SbG5_5W3ZQ"
+							data-src="https://i1-kinhdoanh.vnecdn.net/2023/07/29/dien-mat-troi-mai-nha-8835-1690645255.jpg?w=680&amp;h=0&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=iXXLmHw_Z_97SbG5_5W3ZQ"
+							data-ll-status="loaded">
+					</picture>
+				</div>
+				<figcaption itemprop="description">
+					<p class="Image">Hệ thống điện mặt trời mái nhà được lắp trên nóc một trụ sở doanh nghiệp tại TP
+						HCM. Ảnh: <em>EVNHCM</em></p>
+				</figcaption>
+			</figure>
+			<div id="admbackground" data-set="dfp">
+				<div id="video_outstream" style="display: none;">
+					<div class="txt_ads">Quảng cáo</div>
+					<div id="sis_outstream" style="text-align: center; display: none;"
+						data-google-query-id="CKbk7ZDAtYADFZsVewcdDJIAvw">
+						<div id="google_ads_iframe_/27973503/Vnexpress/Desktop/Outstream/Kinhdoanh/kinhdoanh.vimo.detail_0__container__"
+							style="border: 0pt none;"></div>
+					</div>
+				</div>
+			</div>
+			<p>Theo Quy hoạch điện VIII được phê duyệt, đến 2030 có 50% các tòa nhà công sở, 50% nhà dân
+				sử dụng điện mặt trời mái nhà tự sản, tự tiêu (tiêu thụ tại chỗ, không bán điện vào hệ thống điện quốc
+				gia). Quy hoạch này cũng đề cập cần có phương án ưu tiên, chính sách đột phá để thúc đẩy phát triển điện
+				mặt trời mái nhà của người dân, mái công trình xây dựng, nhất là khu vực có nguy cơ thiếu điện như miền
+				Bắc, và điện mặt trời tự sản, tự tiêu. Công suất điện mặt trời phát triển đến 2030 là 12.836 MW, trong
+				đó nguồn tập trung 10.236 MW, còn điện mặt trời tự sản, tự tiêu khoảng 2.600 MW.</p>
+			<p>Như vậy, theo Quy hoạch điện VIII, đến 2030 công suất điện mặt trời mái nhà (không phân
+				biệt loại hình) và điện mặt trời tự sản, tự tiêu tăng thêm khoảng 2.600 MW. "Với quy mô này thì không
+				cần ban hành cơ chế khuyến khích để lắp đặt điện mặt trời mái nhà", Bộ Công Thương nhận xét.</p>
+			<p>Bởi, chỉ cần khoảng 12,5% nhà dân trên cả nước, mỗi gia đình lắp đặt 1 kW trong năm 2023
+				là đã hoàn thành mục tiêu tăng thêm về cơ cấu nguồn điện mặt trời mái nhà như tại Quy hoạch điện VIII.
+				Đó là chưa kể đến nguồn điện mặt trời mái nhà của các cơ quan công sở, điện mặt trời tự sản tự tiêu tại
+				TP HCM phát triển theo cơ chế đặc thù thí điểm cho thành phố.</p>
+			<p>Vì thế, Bộ Công Thương kiến nghị Thủ tướng cho phép phát triển điện mặt trời mái nhà tự
+				sản, tự tiêu đến năm 2030 với tổng công suất 2.600 MW. Loại nguồn này sẽ được liên kết với lưới điện
+				(đấu nối sau công tơ mua điện) nhưng không phát lên lưới quốc gia, và không bán điện cho tổ chức, cá
+				nhân khác. Điều này xuất phát từ thực tế về vận hành, là trường hợp điện mặt trời không đấu nối với lưới
+				điện quốc gia thì các chủ đầu tư tự cân đối nguồn - tải tại chỗ. Họ sẽ phải lắp thêm lưu trữ điện, làm
+				tăng chi phí đầu tư.</p>
+			<p>Bộ này cũng đề nghị Thủ tướng một số cơ chế khuyến khích đến hết năm 2030, như điện mặt
+				trời mái nhà liên kết với hệ thống điện đã được đấu nối sẽ không phải thỏa thuận đấu nối với ngành điện.
+			</p>
+			<p>Các cơ quan, tổ chức, cá nhân muốn lắp điện mặt trời mái nhà tự sản tự tiêu thì phải đăng
+				ký với UBND cấp tỉnh hoặc cơ quan được giao về quản lý phát triển loại nguồn điện này. Họ sẽ được hưởng
+				chính sách ưu đãi về thuế, phí; ưu tiên bố trí ngân sách để phát triển loại nguồn điện này tại công sở.
+			</p>
+			<p>Tổ chức, cá nhân đầu tư, lắp đặt, sử dụng hệ thống điện mặt trời mái nhà phải đảm bảo các
+				yêu cầu về an toàn điện, công trình xây dựng, môi trường, phòng cháy chữa cháy.</p>
+			<p><strong><span style="color:#2c3e50;">Tuy vậy, các chuyên gia cho rằng những cơ chế Bộ Công
+						Thương đưa ra khó khả thi nếu không lượng hóa</span> <span style="color:#2c3e50;">quy mô cụ
+						thể.</span> </strong>GS. Lê Chí Hiệp, Trường Đại học Bách Khoa TP HCM đồng tình phát triển điện
+				mái nhà "tự sản tự tiêu", song ông lưu ý, nếu phát triển đơn lẻ trên mái từng hộ nhà dân, công sở thì sẽ
+				khó đạt được mục tiêu nguồn điện này đạt tỷ trọng lớn, tham gia vào cán cân năng lượng đất nước.</p>
+			<p>"Cơ quan quản lý cần tính toán và lượng hóa được quy mô phát triển nguồn điện này trong
+				từng giai đoạn. Còn đưa ra một khái niệm chung mà không rõ quy mô, sẽ thất bại", ông nói.</p>
+			<p>Cũng nhận xét cơ chế đưa ra còn chung chung, <a
+					href="https://vnexpress.net/co-che-dien-mat-troi-mai-nha-tu-dung-bi-che-chua-hap-dan-4632922.html"
+					rel="dofollow"
+					data-itm-source="#vn_source=Detail-KinhDoanh_ViMo-4635600&amp;vn_campaign=Box-InternalLink&amp;vn_medium=Link-ChuaHapDanDauTu&amp;vn_term=Desktop&amp;vn_thumb=0&amp;vn_test=B"
+					data-itm-added="1">chưa hấp dẫn đầu tư</a>, Bộ Kế hoạch &amp; Đầu tư đề nghị, làm rõ miễn hoặc giảm
+				các loại thuế, phí cụ thể cho nhà đầu tư. Việc ưu tiên ngân sách cho loại nguồn điện này là vốn đầu tư
+				công hay chi thường xuyên. Trường hợp sử dụng vốn đầu tư công cần phải thực hiện theo trình tự, thủ tục
+				của dự án đầu tư công.</p>
+			<div class="width_common box-tinlienquanv2 ">
+
+				<article class="item-news">
+					<div class="thumb-art">
+						<a href="https://vnexpress.net/co-che-dien-mat-troi-mai-nha-tu-dung-bi-che-chua-hap-dan-4632922.html"
+							data-itm-source="#vn_source=Detail-KinhDoanh_ViMo-4635600&amp;vn_campaign=Box-TinXemThem&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1&amp;vn_test=B"
+							class="thumb thumb-5x3" title="Cơ chế điện mặt trời mái nhà tự dùng bị chê 'chưa hấp dẫn'"
+							data-itm-added="1">
+
+							<picture>
+								<!--[if IE 9]><video style="display: none;"><![endif]-->
+								<source
+									data-srcset="https://i1-kinhdoanh.vnecdn.net/2023/07/23/dienmattroiapmai3-1690102150-1-9390-8601-1690102451.jpg?w=180&amp;h=108&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=oOfFabg9FiMH0O8R1UW3hA 1x, https://i1-kinhdoanh.vnecdn.net/2023/07/23/dienmattroiapmai3-1690102150-1-9390-8601-1690102451.jpg?w=180&amp;h=108&amp;q=100&amp;dpr=2&amp;fit=crop&amp;s=-AMCT6rsLN2m6PtgLyh1LA 2x">
+								<!--[if IE 9]></video><![endif]-->
+								<img loading="lazy" intrinsicsize="180x108"
+									alt="Cơ chế điện mặt trời mái nhà tự dùng bị chê 'chưa hấp dẫn'" class="lazy"
+									src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+									data-src="https://i1-kinhdoanh.vnecdn.net/2023/07/23/dienmattroiapmai3-1690102150-1-9390-8601-1690102451.jpg?w=180&amp;h=108&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=oOfFabg9FiMH0O8R1UW3hA"
+									data-ll-status="observed">
+							</picture>
+
+						</a>
+					</div>
+					<h4 class="title-news"><a
+							href="https://vnexpress.net/co-che-dien-mat-troi-mai-nha-tu-dung-bi-che-chua-hap-dan-4632922.html"
+							data-itm-source="#vn_source=Detail-KinhDoanh_ViMo-4635600&amp;vn_campaign=Box-TinXemThem&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1&amp;vn_test=B"
+							title="Cơ chế điện mặt trời mái nhà tự dùng bị chê 'chưa hấp dẫn'" data-itm-added="1">Cơ chế
+							điện mặt trời mái nhà tự dùng bị chê 'chưa hấp dẫn'</a></h4>
+					<p class="description">
+						<a data-itm-source="#vn_source=Detail-KinhDoanh_ViMo-4635600&amp;vn_campaign=Box-TinXemThem&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1&amp;vn_test=B"
+							href="https://vnexpress.net/co-che-dien-mat-troi-mai-nha-tu-dung-bi-che-chua-hap-dan-4632922.html"
+							title="Cơ chế điện mặt trời mái nhà tự dùng bị chê 'chưa hấp dẫn'" data-itm-added="1">Ông
+							Linh, người định dùng 100 m2 trên mái nhà làm điện mặt trời ở Bình Chánh (TP HCM), đã đổi ý
+							sau khi đọc dự thảo cơ chế khuyến khích của Bộ Công Thương.</a>
+						<span class="meta-news">
+
+							<a class="count_cmt"
+								href="https://vnexpress.net/co-che-dien-mat-troi-mai-nha-tu-dung-bi-che-chua-hap-dan-4632922.html#box_comment_vne"
+								style="white-space: nowrap; display: inline-block;">
+								<svg class="ic ic-comment">
+									<use xlink:href="#Comment-Reg"></use>
+								</svg>
+								<span class="font_icon widget-comment-4632922-1">94</span>
+							</a>
+
+						</span>
+					</p>
+				</article>
+
+				<article class="item-news">
+					<div class="thumb-art">
+						<a href="https://vnexpress.net/de-xuat-co-che-khuyen-khich-dien-mat-troi-tu-dung-tai-nha-cong-so-4618888.html"
+							data-itm-source="#vn_source=Detail-KinhDoanh_ViMo-4635600&amp;vn_campaign=Box-TinXemThem&amp;vn_medium=Item-2&amp;vn_term=Desktop&amp;vn_thumb=1&amp;vn_test=B"
+							class="thumb thumb-5x3"
+							title="Đề xuất cơ chế khuyến khích điện mặt trời tự dùng tại nhà, công sở"
+							data-itm-added="1">
+
+							<picture>
+								<!--[if IE 9]><video style="display: none;"><![endif]-->
+								<source
+									data-srcset="https://i1-kinhdoanh.vnecdn.net/2023/06/18/dienmattroimainha-1687057652-1-5704-3214-1687057994.jpg?w=180&amp;h=108&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=nqSDKOPuB0bmIw_KGYDNfg 1x, https://i1-kinhdoanh.vnecdn.net/2023/06/18/dienmattroimainha-1687057652-1-5704-3214-1687057994.jpg?w=180&amp;h=108&amp;q=100&amp;dpr=2&amp;fit=crop&amp;s=ukx7TLQrngHa-T1N-Mg8DA 2x">
+								<!--[if IE 9]></video><![endif]-->
+								<img loading="lazy" intrinsicsize="180x108"
+									alt="Đề xuất cơ chế khuyến khích điện mặt trời tự dùng tại nhà, công sở"
+									class="lazy"
+									src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+									data-src="https://i1-kinhdoanh.vnecdn.net/2023/06/18/dienmattroimainha-1687057652-1-5704-3214-1687057994.jpg?w=180&amp;h=108&amp;q=100&amp;dpr=1&amp;fit=crop&amp;s=nqSDKOPuB0bmIw_KGYDNfg"
+									data-ll-status="observed">
+							</picture>
+
+						</a>
+					</div>
+					<h4 class="title-news"><a
+							href="https://vnexpress.net/de-xuat-co-che-khuyen-khich-dien-mat-troi-tu-dung-tai-nha-cong-so-4618888.html"
+							data-itm-source="#vn_source=Detail-KinhDoanh_ViMo-4635600&amp;vn_campaign=Box-TinXemThem&amp;vn_medium=Item-2&amp;vn_term=Desktop&amp;vn_thumb=1&amp;vn_test=B"
+							title="Đề xuất cơ chế khuyến khích điện mặt trời tự dùng tại nhà, công sở"
+							data-itm-added="1">Đề xuất cơ chế khuyến khích điện mặt trời tự dùng tại nhà, công sở</a>
+					</h4>
+					<p class="description">
+						<a data-itm-source="#vn_source=Detail-KinhDoanh_ViMo-4635600&amp;vn_campaign=Box-TinXemThem&amp;vn_medium=Item-2&amp;vn_term=Desktop&amp;vn_thumb=1&amp;vn_test=B"
+							href="https://vnexpress.net/de-xuat-co-che-khuyen-khich-dien-mat-troi-tu-dung-tai-nha-cong-so-4618888.html"
+							title="Đề xuất cơ chế khuyến khích điện mặt trời tự dùng tại nhà, công sở"
+							data-itm-added="1">Hộ gia đình, cơ quan công sở lắp điện mặt trời mái nhà để tự dùng tại chỗ
+							sẽ được hưởng một số cơ chế khuyến khích miễn, giảm thuế, phí, vay lãi suất ưu đãi.</a>
+						<span class="meta-news">
+
+							<a class="count_cmt"
+								href="https://vnexpress.net/de-xuat-co-che-khuyen-khich-dien-mat-troi-tu-dung-tai-nha-cong-so-4618888.html#box_comment_vne"
+								style="white-space: nowrap; display: inline-block;">
+								<svg class="ic ic-comment">
+									<use xlink:href="#Comment-Reg"></use>
+								</svg>
+								<span class="font_icon widget-comment-4618888-1">221</span>
+							</a>
+
+						</span>
+					</p>
+				</article>
+
+			</div>
+			<figure class="tplCaption action_thumb_added"></figure> <!-- Hope -->
+			<!-- End Hope -->
+			<p class="author_mail">
+				<strong>Anh Minh</strong>
+			</p>
+		</article>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			newTitle: String
+		}
+	}
+</script>
